ce0a0244d6b5bbd53aeaaed3137831d0ae03147e
Niv Badash <nivbadd@gmail.commkdir -p nginx-devops/{nginx-app,terraform,.github/workflows}cd nginx-devopsgit initgit remote add origin https://github.com/Nibadd/nginx-devops.git~ $ git config --global user.name  Niv>
2025-05-26 13:41:11 +0000

    Fix route block formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,8 +22,13 @@ resource "aws_internet_gateway" "igw" { vpc_id = aws_vpc.main.id }
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0" gateway_id = aws_internet_gateway.igw.id }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
+
 resource "aws_route_table_association" "assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
