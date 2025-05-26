d144c8fb4de7fb6dd2ecd4f363a826d4098e32f8
Niv Badash <nivbadd@gmail.commkdir -p nginx-devops/{nginx-app,terraform,.github/workflows}cd nginx-devopsgit initgit remote add origin https://github.com/Nibadd/nginx-devops.git~ $ git config --global user.name  Niv>
2025-05-26 14:11:43 +0000

    moveo

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,38 @@
-provider "aws" { region = "us-east-1" }
+############################################
+# Provider
+############################################
+provider "aws" {
+  region = "us-east-1"     # change if you prefer another region
+}
 
-# ----- VPC -----
+############################################
+# VPC + Subnets
+############################################
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
 
+# Public subnet (ALB)
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
 
+# Private subnet (EC2)
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
 }
 
-resource "aws_internet_gateway" "igw" { vpc_id = aws_vpc.main.id }
+# Internet gateway for public subnet
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
 
+# Public route table → IGW
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -29,45 +42,76 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "assoc" {
+# Associate public subnet with the public RT
+resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# ----- Security Groups -----
+############################################
+# Security groups
+############################################
+# 80/tcp from Internet to ALB
 resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.main.id
-  ingress { from_port=80 to_port=80 protocol="tcp" cidr_blocks=["0.0.0.0/0"] }
-  egress  { from_port=0  to_port=0  protocol="-1"  cidr_blocks=["0.0.0.0/0"] }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+# 80/tcp only from ALB SG to EC2
 resource "aws_security_group" "ec2_sg" {
   vpc_id = aws_vpc.main.id
+
   ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }
-  egress { from_port=0 to_port=0 protocol="-1" cidr_blocks=["0.0.0.0/0"] }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-# ----- EC2 -----
+############################################
+# EC2 in private subnet
+############################################
 resource "aws_instance" "nginx" {
-  ami           = "ami-0c2b8ca1dad447f8a"   # us-east-1 Amazon Linux 2
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.private.id
+  ami                    = "ami-0c2b8ca1dad447f8a"        # Amazon Linux 2 (us-east-1)
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
-  user_data = <<-UD
+  user_data = <<-USERDATA
     #!/bin/bash
     yum update -y
     amazon-linux-extras install docker -y
     service docker start
     docker run -d -p 80:80 nivbadasshboss/custom-nginx:latest
-  UD
+  USERDATA
+
+  tags = { Name = "nginx-private" }
 }
 
-# ----- ALB -----
+############################################
+# Application Load Balancer (public)
+############################################
 resource "aws_lb" "alb" {
   name               = "nginx-alb"
   load_balancer_type = "application"
@@ -83,18 +127,20 @@ resource "aws_lb_target_group" "tg" {
   target_type = "instance"
 }
 
-resource "aws_lb_target_group_attachment" "att" {
+resource "aws_lb_target_group_attachment" "attachment" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.nginx.id
   port             = 80
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
+
